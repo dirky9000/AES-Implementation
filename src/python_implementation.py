8a8720ca78c533019eff796dc29220fd3f6df723
Dirk Catpo Risco <dcatpo@hotmail.com>
2023-09-26 21:51:49 -0400

    maybe making a new python file tthat will be less code

--- src/python_implementation.py
+++ src/python_implementation.py
@@ -9,7 +9,7 @@ class AESCipher(object):
         self.key = hashlib.sha256(key.encode()).digest()
 
     def encrypt(self, plain_text):
-        plain_text = self.__pad(plain_text)
+        plain_text = self.pad(plain_text)
         iv = Random.new().read(self.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         encrypted_text = cipher.encrypt(plain_text.encode())
@@ -24,11 +24,11 @@ class AESCipher(object):
             iv = encrypted_text[:self.block_size]
             cipher = AES.new(self.key, AES.MODE_CBC, iv)
             plain_text = cipher.decrypt(encrypted_text[self.block_size:]).decode("utf-8")
-            return self.__unpad(plain_text)
+            return self.unpad(plain_text)
         except (ValueError, KeyError, IndexError):
             return "Decryption failed (incorrect key)"
 
-    def __pad(self, plain_text):
+    def pad(self, plain_text):
         number_of_bytes_to_pad = self.block_size - len(plain_text) % self.block_size
         ascii_string = chr(number_of_bytes_to_pad)
         padding_str = number_of_bytes_to_pad * ascii_string
@@ -36,50 +36,56 @@ class AESCipher(object):
         return padded_plain_text
 
     @staticmethod
-    def __unpad(plain_text):
+    def unpad(plain_text):
         last_character = plain_text[len(plain_text) - 1:]
         return plain_text[:-ord(last_character)]
 
-if __name__ == "__main__":
-    # Prompt the user to enter the secret key
-    encryption_key = input("Enter the secret key: ")
+while True:
+    # Prompt user to enter secret key which must be 16 characters long 
+    secret_key = input("\nEnter secret key (16 characters): ")
 
-    # Create an instance of AESCipher with the provided key
-    cipher = AESCipher(encryption_key)
+    # Check if secret key length is exactly 16 characters
+    if len(secret_key) != 16:
+        print("\nSecret key must be exactly 16 characters long.")
+    else:
+        break  
 
-    while True:
-        # Prompt the user to enter the plaintext (must be 16 characters long)
-        plaintext = input("Enter plaintext (16 characters): ")
+# Create an instance of AESCipher with the provided key
+cipher = AESCipher(secret_key)
 
-        # Check if the plaintext length is exactly 16 characters
-        if len(plaintext) != 16:
-            print("Plaintext must be exactly 16 characters long.")
+while True:
+    # Prompt user to enter plaintext which must be 16 characters long 
+    plaintext = input("Enter plaintext (16 characters): ")
+
+    # Check if plaintext length is exactly 16 characters
+    if len(plaintext) != 16:
+        print("\nPlaintext must be exactly 16 characters long.")
+    else:
+        break 
+
+# Encrypt the plaintext
+encrypted_text = cipher.encrypt(plaintext)
+
+# Print the encrypted text
+print("\nEncrypted text:", encrypted_text)
+
+while True:
+    # Prompt user to enter encrypted text for decryption
+    encrypted_input = input("\nEnter encrypted text: ")
+
+    # Prompt user to enter secret key for decryption
+    secret_key = input("Enter secret key for decryption: ")
+
+    # Decrypt ciphertext to plaintext
+    decrypted_text = cipher.decrypt(encrypted_input, secret_key)
+
+    # Checking if decrypted text is the same as original plaintext
+    if decrypted_text != "Decryption failed (incorrect key)":
+        print("\nDecrypted text:", decrypted_text)
+        if decrypted_text == plaintext:
+            print("Decryption matches original plaintext\n")
         else:
-            break  # Exit the loop if valid input is provided
-
-    # Encrypt the plaintext
-    encrypted_text = cipher.encrypt(plaintext)
-
-    # Print the encrypted text
-    print("Encrypted text:", encrypted_text)
-
-    while True:
-        # Prompt the user to enter the encrypted text for decryption
-        encrypted_input = input("Enter the encrypted text: ")
-
-        # Prompt the user to enter the secret key for decryption
-        secret_key = input("Enter the secret key for decryption: ")
-
-        # Decrypt the ciphertext back to plaintext
-        decrypted_text = cipher.decrypt(encrypted_input, secret_key)
-
-        # Check if decryption was successful
-        if decrypted_text != "Decryption failed (incorrect key)":
-            print("Decrypted text:", decrypted_text)
-            if decrypted_text == plaintext:
-                print("Decryption matched the original plaintext.")
-            else:
-                print("Decryption did not match the original plaintext.")
-            break
-        else:
-            print("Decryption failed. Incorrect key entered.")
+            print("Decryption did not match original plaintext\n")
+        break
+    else:
+        print("Decryption failed, incorrect key entered\n")
