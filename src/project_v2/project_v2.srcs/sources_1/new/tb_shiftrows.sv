e05b05397aa33dd9a5c6f6865a86d08c66d168cf
Dirk Catpo Risco <112519537+dirky9000@users.noreply.github.com>
2023-12-04 13:34:38 -0500

    Update tb_shiftrows.sv

--- src/project_v2/project_v2.srcs/sources_1/new/tb_shiftrows.sv
+++ src/project_v2/project_v2.srcs/sources_1/new/tb_shiftrows.sv
@@ -1,20 +1,22 @@
-`timescale 1ns / 1ps
-
-module tb;
+module tb_shiftrows;
 
 // Signals
 reg clk;
+reg reset; // Change signal name to reflect active-low convention
 reg [7:0] data;
 reg [127:0] data_in;
 wire [127:0] shiftrows_output;
+wire [7:0] sbox_output;
 
 // Instantiate the modules
 sbox sbox_inst(
-    .data(data)
+    .data(data),
+    .dout(sbox_output)
 );
 
 shiftrows shiftrows_inst(
     .clk(clk),
+    .reset(reset), // Change signal name to reflect active-low convention
     .data_in(data_in),
     .data_out(shiftrows_output)
 );
@@ -22,7 +24,7 @@ shiftrows shiftrows_inst(
 // Clock generation
 initial begin
     clk = 0;
-    forever #5 clk = ~clk;  // Using a default time for clock toggling
+    forever #10 clk = ~clk;  // Using a longer time for clock toggling
 end
 
 // Test stimulus
@@ -30,14 +32,17 @@ initial begin
     // Initialize inputs
     data = 8'h23;
     data_in = 128'h696C6F766574686973636C6173732121;
+    reset = 1; // Change signal name to reflect active-low convention
 
-    // Apply inputs and monitor outputs
-    repeat (20) begin
-        #5 data = data + 1; // Change input data
-    end
+    // Apply inputs
+    #10 reset = 0; // Change signal name to reflect active-low convention
+    #10 reset = 1; // Change signal name to reflect active-low convention
+
+    // Monitor outputs for debugging
+    $monitor("Time=%0t, data=%h, data_in=%h, sbox_output=%h, shiftrows_output=%h", $time, data, data_in, sbox_output, shiftrows_output);
 
     // Stop simulation
-    $stop;
+    #100 $stop;
 end
 
 endmodule
